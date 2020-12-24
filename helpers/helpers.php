430c3eefb91762aad7c2eced245fa10bc8b679ab
Dongnguyen <dongnguyen@b13technology.com>
2020-12-24 10:05:43 +0700

    Update

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,10 +1,77 @@
 <?php
 
 use Dnsoft\Eav\EavServiceProvider;
+use Dnsoft\Eav\Models\AttributeOption;
+use Dnsoft\Eav\Repositories\AttributeRepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Str;
-use Dnsoft\Eav\Repositories\AttributeRepositoryInterface;
+
+if (!function_exists('eav_entity')) {
+    /**
+     * Get EAV Entity Collection
+     *
+     * @return Collection|mixed
+     */
+    function eav_entity()
+    {
+        return app('rinvex.attributes.entities');
+    }
+}
+
+if (!function_exists('eav_attribute')) {
+    /**
+     * Get EAV Attribute Model
+     *
+     * @return \Dnsoft\Eav\Models\Attribute|Builder
+     */
+    function eav_attribute()
+    {
+        return app(AttributeRepositoryInterface::class);
+    }
+}
+
+if (!function_exists('get_attribute_option_values')) {
+    /**
+     * Get Attribute Option Values
+     *
+     * @param  \Dnsoft\Eav\Models\Attribute  $attribute
+     * @return array
+     */
+    function get_attribute_option_values($attribute): array
+    {
+        $items = [];
+
+        foreach ($attribute->options as $option) {
+            $items[] = [
+                'id'    => $option->id,
+                'label' => $option->value,
+                'value' => $option->id,
+            ];
+        }
+
+        return $items;
+    }
+}
+
+if (!function_exists('get_attribute_option_value')) {
+    /**
+     * Get attribute option value from eav
+     *
+     * @param $optionId
+     * @return string
+     */
+    function get_attribute_option_value($optionId): string
+    {
+        $attributeOption = AttributeOption::find($optionId);
+        if ($attributeOption) {
+            return $attributeOption->value;
+        }
+
+        return $optionId;
+    }
+}
 
 if (!function_exists('get_attribute_input_type_options')) {
     /**
@@ -12,7 +79,7 @@ if (!function_exists('get_attribute_input_type_options')) {
      *
      * @return array
      */
-    function get_attribute_input_type_options()
+    function get_attribute_input_type_options(): array
     {
         return collect(EavServiceProvider::$mapInputType)
             ->map(function ($item, $key) {
@@ -32,7 +99,7 @@ if (!function_exists('get_attribute_input_type_label')) {
      * @param $key
      * @return string
      */
-    function get_attribute_input_type_label($key)
+    function get_attribute_input_type_label($key): string
     {
         $tranKey = 'eav::attribute.map_input_type.'.$key;
 
@@ -47,7 +114,7 @@ if (!function_exists('get_all_attributes_of_type')) {
      * @param $entityType
      * @return Collection
      */
-    function get_all_attributes_of_type($entityType)
+    function get_all_attributes_of_type($entityType): Collection
     {
         return app(AttributeRepositoryInterface::class)->all($entityType);
     }
